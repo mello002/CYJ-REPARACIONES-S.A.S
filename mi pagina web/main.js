fa774e633613cb3cde6ad5096a9e1808942bdff4
mello002 <56882055+mello002@users.noreply.github.com>
2023-03-26 23:00:19 -0500

    Add files via upload

---
+++ mi pagina web/main.js
@@ -0,0 +1,13 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase',{delay:500});
+ScrollReveal().reveal('.titulo-servicio',{delay:500});
+ScrollReveal().reveal('.about-service',{delay:500});
+ScrollReveal().reveal('.contacto',{delay:500})
+ScrollReveal().reveal('.footer');
+
+
+
+
